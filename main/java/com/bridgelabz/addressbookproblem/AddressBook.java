cb819c62d248e8a3ca6a117c1be08cdf9ecd288c
shindeanant <shindeanant1997@gmail.com>
2021-10-25 01:10:29 +0530

    Added: Refactor to add no duplicate of the same person.

--- main/java/com/bridgelabz/addressbookproblem/AddressBook.java
+++ main/java/com/bridgelabz/addressbookproblem/AddressBook.java
@@ -92,13 +92,13 @@ public class AddressBook {
 		 * This method is used to read contacts
 		 *
 		 * @return returnscontact
+		 * @param firstName
+		 * @param lastName
 		 */
-		public Contact readContactDetails() {
+		public Contact readContactDetails(String firstName, String lastName) {
 			Contact contact = new Contact();
-			System.out.println("Enter FN");
-			contact.setFirstName(sc.nextLine());
-			System.out.println("Enter LN");
-			contact.setLastName(sc.nextLine());
+			contact.setFirstName(firstName);
+			contact.setLastName(lastName);
 			System.out.println("Enter Add");
 			contact.setAddress(sc.nextLine());
 			System.out.println("Enter City");
@@ -129,10 +129,13 @@ public class AddressBook {
 
 		/**
 		 * this method is used to add contacts
+		 * 
+		 * @param firstName
+		 * @param lastName
 		 */
 
-		public void addContact() {
-			Contact contact = this.readContactDetails();
+		public void addContact(String firstName, String lastName) {
+			Contact contact = this.readContactDetails(firstName, lastName);
 			this.contacts.add(contact);
 		}
 
@@ -150,7 +153,7 @@ public class AddressBook {
 				System.out.println("Contact Not Found");
 			} else {
 				System.out.println("Enter new Values");
-				Contact contact = this.readContactDetails();
+				Contact contact = this.readContactDetails(firstName, lastName);
 				this.contacts.set(index, contact);
 			}
 		}
@@ -211,8 +214,18 @@ public class AddressBook {
 
 				switch (option) {
 				case 1:
-					this.addContact();
-					break;
+					System.out.println("Enter FN");
+					String firstName = sc.nextLine();
+					System.out.println("Enter LN");
+					String lastName = sc.nextLine();
+					int index = search(firstName, lastName);
+					if (index == -1) {
+						this.addContact(firstName, lastName);
+						break;
+					} else {
+						System.out.println("Entry Already exists");
+						break;
+					}
 				case 2:
 					this.editContact();
 					break;
@@ -305,6 +318,5 @@ public class AddressBook {
 					+ city + ", state=" + state + ", zip=" + zip + ", phoneNumber=" + phoneNumber + ", emailId="
 					+ emailId + "]";
 		}
-
 	}
 }
